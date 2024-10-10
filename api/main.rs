f221d955fa03ea699304e5ecb9ce8d14cbff56db
duytran <duyquangtran@kms-technology.com>
2024-10-10 02:14:29 -0500

    Add function search and use elasticsearch to store log

--- api/main.rs
+++ api/main.rs
@@ -1,6 +1,7 @@
 extern crate pretty_env_logger;
 
 use dotenv::dotenv;
+use elasticsearch::{http::transport::Transport, Elasticsearch, SearchParts};
 use serde::{Deserialize, Serialize};
 use serde_json::{json, Value};
 use sqlx::postgres::{PgPool, PgPoolOptions};
@@ -41,6 +42,40 @@ async fn get_posts(pool: PgPool) -> Result<impl Reply, Rejection> {
     }
 }
 
+async fn search_posts(query: SearchQuery, es: Elasticsearch) -> Result<impl Reply, Rejection> {
+    let response = es
+        .search(SearchParts::Index(&["crawler-posts"]))
+        .body(json!({
+            "query": {
+                "query_string": query
+            }
+        }))
+        .send()
+        .await
+        .map_err(|error| {
+            eprintln!("Elasticsearch request failed: {}", error);
+            warp::reject::reject()
+        })?;
+
+    let body = response.json::<Value>().await.map_err(|error| {
+        eprintln!("Failed to parse Elasticsearch response: {}", error);
+        warp::reject()
+    })?;
+
+    let mut v = Vec::new();
+    for hit in body["hits"]["hits"].as_array().unwrap() {
+        let id = hit["_source"]["after"]["id"].clone();
+
+        if v.contains(&id) {
+            continue;
+        }
+
+        v.push(id);
+    }
+
+    Ok(warp::reply::json(&v))
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     if env::var_os("RUST_LOG").is_none() {
@@ -55,10 +90,20 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .connect(&env::var("POSTGRES_DNS").unwrap())
         .await?;
 
+    let transport = Transport::single_node(&env::var("ELASTICSEARCH_DNS").unwrap())?;
+    let client = Elasticsearch::new(transport);
+
     let get_posts = warp::get()
         .and(warp::path("posts"))
         .and(with_db(pool.clone()))
         .and_then(get_posts);
+
+    let search_posts = warp::post()
+        .and(warp::path("posts"))
+        .and(warp::path("search"))
+        .and(json_body())
+        .and(with_es(client.clone()))
+        .and_then(search_posts);
 
     let options_route = warp::options().map(warp::reply);
 
@@ -68,6 +113,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .allow_headers(vec!["Content-Type"]);
 
     let routes = get_posts
+        .or(search_posts)
         .or(options_route)
         .with(cors)
         .with(warp::log("posts"));
@@ -81,4 +127,14 @@ fn with_db(
     pool: PgPool,
 ) -> impl Filter<Extract = (PgPool,), Error = std::convert::Infallible> + Clone {
     warp::any().map(move || pool.clone())
+}
+
+fn with_es(
+    es: Elasticsearch,
+) -> impl Filter<Extract = (Elasticsearch,), Error = std::convert::Infallible> + Clone {
+    warp::any().map(move || es.clone())
+}
+
+fn json_body() -> impl Filter<Extract = (SearchQuery,), Error = warp::Rejection> + Clone {
+    warp::body::content_length_limit(1024 * 16).and(warp::body::json())
 }
